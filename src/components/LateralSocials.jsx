920f4ce1378b75820ea6a18a1e603af53501c7a6
devandrewjpn <yuutoandrew.jpn@gmail.com>
2024-01-25 15:53:45 -0300

    changing image and redes

--- src/components/LateralSocials.jsx
+++ src/components/LateralSocials.jsx
@@ -4,13 +4,13 @@ const LateralSocials = () => {
   return (
     <>
       <section className="skll__socials">
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/profile.php?id=61555764308346" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-facebook-f" />
         </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.tiktok.com/@usoseguros" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-tiktok" />
         </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/usoseguros/" target="_blank" rel="noopener noreferrer">
             <i className="fa-brands fa-instagram" />
         </a>
       </section>
