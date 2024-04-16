286749a772f9b12bb3e1b66f58ee4aabe200c8cf
Design <design@Macs-MBP.intelbras.local>
2024-04-16 10:34:12 -0300

    uso saude alteracao

--- src/components/SaudePricing.jsx
+++ src/components/SaudePricing.jsx
@@ -5,7 +5,7 @@ const items = [
     {
         id: 1,
         title: 'USO SAÚDE I - Proteção Essencial',
-        value: 33,
+        value: "33,97",
         redirect: 'https://usoseguros.plataformadeseguros.com.br/',
         description: "O básico não precisa ser básico",
         label: "Médico na Tela para até 4 membros da família",
@@ -21,17 +21,35 @@ const items = [
         ],
         assistances: [
             {
+                id: 4,
+                text: 'Médico na Tela Familiar'
+            },
+            {
+                id: 5,
+                text: 'Rede de Desconto em Farmácia'
+            },
+            {
+                id: 6,
+                text: 'Funeral Familiar'
+            },
+            // {
+            //     id: 1,
+            //     text: 'Desconto Farmácia'
+            // },
+            // {
+            //     id: 2,
+            //     text: 'Assistência Funeral Individual - Até R$5.000'
+            // },
+            // {
+            //     id: 3,
+            //     text: 'Assistência Pessoal'
+            // },
+        ],
+        sort: [
+            {
                 id: 1,
-                text: 'Desconto Farmácia'
-            },
-            {
-                id: 2,
-                text: 'Assistência Funeral Individual - Até R$5.000'
-            },
-            {
-                id: 3,
-                text: 'Assistência Pessoal'
-            },
+                text: 'Sorteiro Mensal R$20.000 (Isento de Impostos) '
+            }
         ],
         alert: [
             {
@@ -43,7 +61,7 @@ const items = [
     {
         id: 2,
         title: 'USO SAÚDE II - Proteção Ampliada',
-        value: 50,
+        value: "63,75",
         redirect: 'https://usoseguros.plataformadeseguros.com.br/',
         description: "O dobro de cobertura",
         label: "Médico na Tela para até 4 membros da família",
@@ -59,17 +77,35 @@ const items = [
         ],
         assistances: [
             {
+                id: 4,
+                text: 'Médico na Tela Familiar'
+            },
+            {
+                id: 5,
+                text: 'Rede de Desconto em Farmácia'
+            },
+            {
+                id: 6,
+                text: 'Funeral Familiar'
+            },
+            // {
+            //     id: 1,
+            //     text: 'Desconto Farmácia'
+            // },
+            // {
+            //     id: 2,
+            //     text: 'Assistência Funeral Individual - Até R$5.000'
+            // },
+            // {
+            //     id: 3,
+            //     text: 'Assistência Pessoal'
+            // },
+        ],
+        sort: [
+            {
                 id: 1,
-                text: 'Desconto Farmácia'
-            },
-            {
-                id: 2,
-                text: 'Assistência Funeral Individual - Até R$5.000'
-            },
-            {
-                id: 3,
-                text: 'Assistência Pessoal'
-            },
+                text: 'Sorteiro Mensal R$20.000 (Isento de Impostos) '
+            }
         ],
         alert: [
             {
@@ -81,46 +117,58 @@ const items = [
     {
         id: 3,
         title: 'USO SAÚDE III - Proteção Completa',
-        value: 70,
+        value: "79,27",
         redirect: 'https://usoseguros.plataformadeseguros.com.br/',
         description: "Mais cobertura, mais benefícios e a sorte do seu lado",
         label: "Médico na Tela para até 4 membros da família",
         covers: [
             {
                 id: 1,
-                text: 'Morte Acidental - R$200.000'
+                text: 'Morte Acidental - R$250.000'
             },
             {
                 id: 2,
-                text: 'Invalidez Permanente Total por Acidente - R$200.000'
+                text: 'Invalidez Permanente Total por Acidente - R$250.000'
             },
         ],
         assistances: [
             {
-                id: 1,
-                text: 'Desconto Farmácia'
+                id: 6,
+                text: 'Médico na Tela Familiar'
             },
             {
-                id: 2,
-                text: 'Assistência Funeral Individual - Até R$5.000'
+                id: 7,
+                text: 'Rede de Desconto em Farmácia'
             },
             {
-                id: 3,
-                text: 'Assistência Pessoal'
+                id: 8,
+                text: 'Funeral Familiar'
             },
-            {
-                id: 4,
-                text: 'Assistência Funeral  Familiar Até R$7.000'
-            },
-            {
-                id: 5,
-                text: 'Vítima de Crime'
-            }
+            // {
+            //     id: 1,
+            //     text: 'Desconto Farmácia'
+            // },
+            // {
+            //     id: 2,
+            //     text: 'Assistência Funeral Individual - Até R$5.000'
+            // },
+            // {
+            //     id: 3,
+            //     text: 'Assistência Pessoal'
+            // },
+            // {
+            //     id: 4,
+            //     text: 'Assistência Funeral  Familiar Até R$7.000'
+            // },
+            // {
+            //     id: 5,
+            //     text: 'Vítima de Crime'
+            // }
         ],
         sort: [
             {
                 id: 1,
-                text: 'Sorteiro Mensal R$200.000 (Isento de Impostos)'
+                text: 'Sorteiro Mensal R$20.000 (Isento de Impostos)'
             }
         ],
         alert: [
